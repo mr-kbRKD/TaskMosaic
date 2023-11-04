c9693fb581e6e716c0252f038f305cecbe6d1fd3
KULBHUSHAN <krishnathakur0007895@gmail.com>
2023-11-04 10:43:11 +0530

    Frontend of project completed

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <button className='btn'>
+        Add More
+    </button>
+  )
+}
+
+export default Button;
---
+++ src/components/Todoitem.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Todoitem = () => {
+  return (
+    <>
+    <li className='todoitem'>
+    <span>
+      <input type="checkbox" name="" id="" />
+      <span className='todo-item-text'>Eat</span>
+    </span>
+      <p>...</p>
+    </li>
+    </>
+  )
+}
+
+export default Todoitem;
